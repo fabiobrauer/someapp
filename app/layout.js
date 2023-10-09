eeb2d9457fee58b509ffc6128f7915695e14d8d9
fabiobrauer <brauerfabiio@gmail.com>
2023-10-09 17:41:32 +0200

    google verify

--- app/layout.js
+++ app/layout.js
@@ -7,12 +7,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Agroforst-Norddeutschland',
-  description: 'Made By Fabio',
+  description: 'Agroforst Norddeutschland',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name='description' content='Permakultur + Garten'/>
+        <meta name='description' content='Andrea Pflaum Agroforst'/>
+        <meta name="google-site-verification" content="yZ5WrG4kYxlz5scnE2vOrESjdRVRttIL9420ZOMUAFA" />
+      </head>
       <Header></Header>
       <body className={inter.className}>{children}</body>
       <Footer></Footer>
